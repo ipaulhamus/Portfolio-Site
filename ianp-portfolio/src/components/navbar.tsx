1adbe55576246d42b19a21e2af444d08e16b11ac
ianp <iepaulhamus@gmail.com>
2026-03-18 19:49:18 -0400

    Initial project commit

---
+++ ianp-portfolio/src/components/navbar.tsx
@@ -0,0 +1,18 @@
+
+
+export default function NavBar() {
+    return 
+    <>
+        <div className="navbar-container">
+            <h1 className="navbar-logo-text">IP</h1>
+
+            <a className="nav-item">About</a>
+            <a className="nav-item">Skills</a>
+            <a className="nav-item">Projects</a>
+            <a className="nav-item">Experience</a>
+            <a className="nav-item">Hobbies</a>
+            <a className="nav-item">Contact</a>
+        </div>
+    </>
+}
+
